30b958f27c3b1bcdbaf7aa1fcc12d4c2d1961b76
Kmoran1512 <kmoran.1512@gmail.com>
2021-11-10 08:38:13 -0800

    import equal-vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import Equal from 'equal-vue'
+import 'equal-vue/dist/style.css'
 import "./registerServiceWorker";
 
+createApp(App).use(Equal);
 createApp(App).mount("#app");
