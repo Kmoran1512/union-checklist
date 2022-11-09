c5c6ce3c5415d6f6b71ac88cd66a59b8575336c2
Kmoran1512 <kmoran.1512@gmail.com>
2022-11-09 07:16:31 -0500

    deploy

---
+++ precache-manifest.5a57667383e5fc922687f83c294f0190.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f2c89cf06495f6a511",
+    "url": "/union-checklist/css/app.675db5e9.css"
+  },
+  {
+    "revision": "e50f9c3cb6936ea231ff",
+    "url": "/union-checklist/css/chunk-vendors.9742458b.css"
+  },
+  {
+    "revision": "943c3597cd33be56d53df0d1982fa8ff",
+    "url": "/union-checklist/fonts/primeicons.943c3597.woff"
+  },
+  {
+    "revision": "b29a888ff7f07091c7e08eb0d991e221",
+    "url": "/union-checklist/fonts/primeicons.b29a888f.ttf"
+  },
+  {
+    "revision": "e01fd4133bac49cd2ea07ad6f7c45695",
+    "url": "/union-checklist/fonts/primeicons.e01fd413.eot"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/union-checklist/img/color.c7a33805.png"
+  },
+  {
+    "revision": "64b5d470af63a67aa9b9d3e8a866a35e",
+    "url": "/union-checklist/img/primeicons.64b5d470.svg"
+  },
+  {
+    "revision": "6211f4695dbbd0fd7318dd55a5934850",
+    "url": "/union-checklist/index.html"
+  },
+  {
+    "revision": "a7f2c89cf06495f6a511",
+    "url": "/union-checklist/js/app.5121b00a.js"
+  },
+  {
+    "revision": "e50f9c3cb6936ea231ff",
+    "url": "/union-checklist/js/chunk-vendors.57eb87ed.js"
+  },
+  {
+    "revision": "4454b0fa76bbd041f2703d1d441a5b01",
+    "url": "/union-checklist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/union-checklist/robots.txt"
+  }
+]);
